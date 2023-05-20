dfdf11aac33dea8dc4ced69b11bd2d2b7122426d
Stephen Reaves <reaves735@gmail.com>
2023-05-20 07:34:59 -0400

    Add rust test template

---
+++ nvim/Templates/Rust/test.rs
@@ -0,0 +1,14 @@
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = "";
+
+        let result = "";
+
+        assert_eq!(input, result);
+    }
+}
